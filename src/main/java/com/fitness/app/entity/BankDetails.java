a9e657adbcc22a5fd50729d6786c135cc75a3bed
Manish Singh <manishsinghonline2@gmail.com>
2022-02-18 00:04:11 +0530

    password encoded

--- src/main/java/com/fitness/app/entity/BankDetails.java
+++ src/main/java/com/fitness/app/entity/BankDetails.java
@@ -5,4 +5,38 @@ public class BankDetails {
     private String bankName;
     private Long accountNubmer;
     private String bankIFSC;
+
+    public BankDetails() {
+    }
+
+    public BankDetails(String bankName, Long accountNubmer, String bankIFSC) {
+        this.bankName = bankName;
+        this.accountNubmer = accountNubmer;
+        this.bankIFSC = bankIFSC;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public Long getAccountNubmer() {
+        return accountNubmer;
+    }
+
+    public void setAccountNubmer(Long accountNubmer) {
+        this.accountNubmer = accountNubmer;
+    }
+
+    public String getBankIFSC() {
+        return bankIFSC;
+    }
+
+    public void setBankIFSC(String bankIFSC) {
+        this.bankIFSC = bankIFSC;
+    }
+
 }
