9f2ded14d1821c2df4d3878aeb90a4fede8b4e26
manishsinghgithub <manishsinghonline2@gmail.com>
2022-03-01 00:00:25 +0530

    Add_Gym Api done

---
+++ src/main/java/com/fitness/app/entity/WorkoutClass.java
@@ -0,0 +1,15 @@
+package com.fitness.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkoutClass {
+	
+	private String workout_name;
+	private String prefered_time;
+
+}
